a4864884f8638eb6749b1e9cdeffd4e5f3b699ca
Alexandre <aalber200@caledonian.ac.uk>
2013-05-17 16:32:59 +0200

    refactoring

--- Práctica 03/ejercicio 01/lib.js
+++ Práctica 03/ejercicio 01/lib.js
@@ -4,11 +4,15 @@ function toUp(el){
 	el.value = el.value.toUpperCase();
 }
 
-function ValidarNombre(){
- 	var element = document.getElementById("name");
- 	var x=element.value;
 
-	if (x==null || x=="") {
+
+
+
+function validateName(){
+ 	var element = document.getElementById("name");
+ 	var name=element.value;
+
+	if (name==null || name=="") {
 	  	document.getElementById("error_message").innerHTML = "El campo nombre no puede estar en blanco";
 	  	element.focus();
 	  	return false;
@@ -29,6 +33,7 @@ function ValidarApellidos(){
   	document.getElementById("error_message").innerHTML = "";
   	return true;
 }
+
 
 function ValidarEdad(){
 	var element = document.getElementById("age");
@@ -51,26 +56,81 @@ function ValidarEdad(){
 }
 
 
-function ValidarNIF(){
-	var element = document.getElementById("nif");
 
 
-	document.getElementById("error_message").innerHTML = "";
+
+function validateFieldNotBlank(fieldId){
+
+	var errorMessage = document.getElementById("error_message").innerHTML;
+ 	var field = document.getElementById(fieldId);
+ 	var fieldValue = field.value;
+ 	var fieldName  = field.name;
+ 	if (field == null || fieldValue == null || fieldValue == "") {
+	  	document.getElementById("error_message").innerHTML = "El campo <font color='red'>"+fieldName+" </font>no puede estar en blanco";
+	  	field.focus();
+	  	return false;
+  	}
+
+	if (fieldName == 'name'){
+
+		if (fieldValue.length < 2){
+			errorMessage = "El campo <font color='red'>"+fieldName+" </font> minimo de 2 letras";
+		  	field.focus();
+
+			return false;
+		}
+
+
+	}
+
+	if (fieldName == 'surname'){
+		alert('Test Name');
+	}
+
+	if (fieldName == 'age'){
+		alert('Test Name');
+	}
+
+	if (fieldName == 'nif'){
+		alert('Test Name');
+	}
+
+
+  	document.getElementById("error_message").innerHTML = "";
 	return true;
 }
 
 
-function ValidarEmail(){
-	var element = document.getElementById("email");
 
 
-	document.getElementById("error_message").innerHTML = "";
-	return true;
-}
 
 
 function validateAndSubmitForm(){
-	if (!ValidarNombre()){
+
+	var fields = new Array();
+	fields[0] = "name";
+	fields[1] = "surname";
+	fields[2] = "age";
+	fields[3] = "nif";
+	fields[4] = "email";
+	fields[5] = "city";
+	fields[6] = "region";
+	fields[7] = "dob";
+	fields[8] = "phone";
+	fields[9] = "doa";
+	fields[10] = "toa";
+
+	for (i=0;fields.length;i++){
+		if ( !validateFieldNotBlank(fields[i]) ){
+			return;
+		}
+	}
+
+
+
+	/*
+
+	if (!validateName()){
 		return false;
 	}
 
@@ -96,6 +156,7 @@ function validateAndSubmitForm(){
 	var form1 =  document.getElementById('form1');
 	form1.action = 'result.html';
 	form1.submit();
+	*/
 
 
 }
