83c8181c0ad1103d028d765168ad9a777a7cbd5a
Alexandre <aalber200@caledonian.ac.uk>
2013-06-18 14:14:47 +0200

    P04: Create Atributo

---
+++ Practica_04/atributo_new.php
@@ -0,0 +1,59 @@
+<?php include('classes/Atributo.class.php'); ?>
+
+<?php
+
+	$entity = New Atributo();
+
+?>
+
+<html>
+<body>
+
+		<div class="page">
+
+			<div class="main">
+
+				<div class="menu">
+
+					<h1>Nuevo Atributo</h1>
+
+					<br>
+					<form name="form1" method="post"  action="atributo_new_ex.php">
+						<table width="94%" border="1" align="left">
+
+							<tr>
+								<td align="left">Atributo</td>
+								<td align="left">
+									<input type="text" name="atributo" id="atributo" value="<?php echo($entity->atributo); ?>">
+								</td>
+							</tr>
+
+							<tr>
+								<td align="left">Valor</td>
+								<td align="left">
+									<input type="text" name="valor" id="valor" value="<?php echo($entity->valor); ?>">
+								</td>
+							</tr>
+
+							<tr>
+								<td align="left">
+
+								</td>
+								<td align="left">
+									<input type="button" id="cancel_btn" value="Cancelar"> | <input type="button" id="create_btn" value="Crear">
+								</td>
+							</tr>
+						</table>
+
+						<input type="hidden" name="id" id="id" value="<?php echo($entity->id); ?>">
+					</form>
+
+				</div>
+
+
+
+			</div>
+		</div>
+
+</body>
+</html>
