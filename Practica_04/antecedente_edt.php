20bb808e4ef2d6cd4e4a6fb918c19b13c9b9ec19
Alexandre <aalber200@caledonian.ac.uk>
2013-06-20 11:39:57 +0200

    P04: Edit Antecedente Checkbox solved

--- Practica_04/antecedente_edt.php
+++ Practica_04/antecedente_edt.php
@@ -5,6 +5,7 @@
 <?php
 	$entity = New Antecedente();
 	$entity = $entity->read($_GET['id']);
+	$entityAtrLst = $entity->findAtributos();
 
 	$atributoEnt = New Atributo();
 	$atributo_lst = $atributoEnt->readAll();
@@ -26,34 +27,45 @@
 					<form name="form1" method="post"  action="antecedente_save_or_update.php">
 						<input type="hidden" name="id" id="id" value="<?php echo($entity->id); ?>">
 						<table width="94%" border="1" align="left">
-
 							<tr>
 								<td align="left">Atributo / Valor</td>
 								<td align="left">
-									<select name="id_atributo" id="id_atributo">
-
-										<?php
-										$i=0;
-										while($i<count($atributo_lst)){
-											if ($atributo_lst[$i] == $entity->id_atributo){
-												echo('<option value="'.$atributo_lst[$i].'" SELECTED> '.$atributo_lst[$i+1].'='.$atributo_lst[$i+2].'</option>');
-											} else {
-												echo('<option value="'.$atributo_lst[$i].'"> '.$atributo_lst[$i+1].'='.$atributo_lst[$i+2].' </option>');
-											}
-											$i = $i + 3;
-										}
-										?>
-									</select>
 
 								</td>
 							</tr>
 
 
+							<?php
+							$i=0;
+
+							while($i<count($atributo_lst)){
+								$checked = "";
+								$j=1;
+								while($j<count($entityAtrLst)){
+									//echo("while <br>");
+									if ($entityAtrLst[$j] == $atributo_lst[$i]){
+										echo("checked ".$atributo_lst[$i]." <br>");
+										$checked="checked";
+									}
+									$j = $j + 4;
+									//echo("final $j=".$j."<br>");
+								}
+
+								echo('<tr>');
+								echo('	<td> </td>');
+								echo('	<td>');
+								echo('		<input type="checkbox" name="id_atributo[]" id="id_atributo[]" value="'.$atributo_lst[$i].'" '.$checked.'> '.$atributo_lst[$i+1].'='.$atributo_lst[$i+2]);
+								echo('	</td>');
+								echo('</tr>');
+								$i = $i + 3;
+							}
+							?>
+
 							<tr>
 								<td align="left">
 								</td>
 								<td align="left">
-									<input type="button" id="cancel_btn" value="Cancelar" onclick="javascript:location.href='antecedente_lst.php'"> | <input type="submit" id="create_btn" value="Guardar">
+									<input type="button" id="cancel_btn" value="Cancelar" onclick="javascript:location.href='antecedente_lst.php'"> | <input type="submit" id="create_btn" value="Guarder">
 								</td>
 							</tr>
 						</table>
--- Practica_04/antecedente_save_or_update.php
+++ Practica_04/antecedente_save_or_update.php
@@ -4,19 +4,13 @@
 
 	$entity = New Antecedente();
 
-	//$entity->id_atributo = $_POST['id_atributo'];
-
 	$ret = false;
 	if (isset($_POST['id'])){
 		$entity->id = $_POST['id'];
 		$ret = $entity->update();
 	}else {
-		//$ret = $entity->create();
 		$currentId = $entity->findLastId() +1 ;
-
 		foreach($_POST['id_atributo'] as $key => $value){
-			//echo('['.$key.'] = '.$value.'<BR>');
-
 			$ret = $entity->create($currentId,$value);
 		}
 	}
