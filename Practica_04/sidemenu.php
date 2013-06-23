d2fec255ee2997aa4fc7be0be22a766ff08d687d
Alexandre <aalber200@caledonian.ac.uk>
2013-06-23 12:54:41 +0200

    P04: refactoring / added photo to TOP

---
+++ Practica_04/sidemenu.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+
+		<link href="estilo05.css" rel="stylesheet" type="text/css">
+
+	</head>
+
+	<body class="menu_item">
+		<table border="0" cellpadding="0" cellspacing="0">
+			<tr valign="top">
+				<td align="right" class="menu_item_td">
+					&nbsp;
+				</td>
+			</tr>
+			<tr>
+				<td height="1"  colspan="1"  class="white"></td>
+			</tr>
+			<tr valign="top">
+				<td align="right" class="menu_item_td">
+					<a class="menu_item" target="content" href="list_regla.php">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;Inicio&nbsp;&nbsp;</a><br>
+				</td>
+			</tr>
+			<tr valign="top">
+				<td align="right" class="menu_item_td">
+					<a class="menu_item"  target="content"  href="regla_lst.php">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Reglas&nbsp;&nbsp;</a><br>
+				</td>
+			</tr>
+			<tr valign="top">
+				<td align="right" class="menu_item_td">
+					<a class="menu_item"  target="content"  href="antecedente_lst.php">&nbsp;&nbsp; &nbsp;Antecedentes&nbsp;&nbsp;</a><br>
+				</td>
+			</tr>
+			<tr valign="top">
+				<td align="right" class="menu_item_td">
+					<a class="menu_item"  target="content"  href="atributo_lst.php">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;Atributos&nbsp;&nbsp;</a><br>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
--- Practica_04/top.php
+++ Practica_04/top.php
@@ -5,10 +5,16 @@
 
 	<body style="background: #f0e5f8; margin-top: 8px; margin-left: 8px;">
 
-		<h1> Practica 4 - Programacion Web  </h1>
-		<p>
+		<div style="float:LEFT; width=100px;">
+			<img src="images/alexandre_franca_120x160_002.jpg" width="50px">
+		</div>
+
+		<div style="margin-left: 8px;float: left">
+			<h1> Practica 4 - Programacion Web  </h1>
 			Alexandre A Franca /
 			Email: gauchoescoces@correo.ugr.es
-		</p>
+
+		</div>
+
 	</body>
 </html>
